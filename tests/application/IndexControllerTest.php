c75cdd948b2f6218dbbd166d784fbe9fb1528e65
Christian <Christian@core2duo>
2011-05-03 22:12:45 +0200

    Change-Id: Iab74d09bc2cfba827e8b0b485a24604d488ff75b

---
+++ tests/application/IndexControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../TestHelper.php';
+require_once 'Zend/Application.php';
+require_once 'Zend/Test/PHPUnit/ControllerTestCase.php';
+
+class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+	public $application;	// Zend_Application für Bootstrap
+	
+	public function setUp() {
+		$this->bootstrap = new Zend_Application(
+		    APPLICATION_ENV,
+		    APPLICATION_PATH . '/configs/application.ini'
+		);
+		
+		parent::setUp();
+	}
+ 
+	public function testController()
+	{
+		$this->dispatch('/');
+		$this->assertController('Index');
+	}
+}
