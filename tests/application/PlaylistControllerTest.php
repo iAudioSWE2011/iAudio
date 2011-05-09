bdf14e990745aa273b99ae2ca43a1c480d4fa380
Detonator <Detonator@129.187.110.2>
2011-05-09 12:36:51 +0200

    PlaylistControllerTests

--- tests/application/PlaylistControllerTest.php
+++ tests/application/PlaylistControllerTest.php
@@ -33,16 +33,101 @@ class PlaylistControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 	public function testCreateNewPlaylist()
 	{
 		$playlist = new Playlist();
-		$userplaylist = new UserPlaylist();
 		$session = new Session();
-		
 		$name = 'TestPlaylist';
 		
 		$this->dispatch('Playlist/create/name/'.$name);
 		
 		//does the playlist exists?
 		$this->assertTrue($playlist->existsForUser($name, $session->getUserID(session_id())));
+		$this->dispatch('Playlist/delete/name/'.$name);
 	}
+	
+	
+	public function testNoDoublePlaylists()
+	{
+		$playlist = new Playlist();
+		$session = new Session();
+		$name = 'TestPlaylist';
+		
+		$this->dispatch('Playlist/create/name/'.$name);
+		
+		//does the playlist exists?
+		$this->assertTrue($playlist->existsForUser($name, $session->getUserID(session_id())));
+		
+		$amount = count($playlist->getPlaylists($session->getUserID(session_id())));
+		
+		$this->dispatch('Playlist/create/name/'.$name);
+		
+		$amountNew = count($playlist->getPlaylists($session->getUserID(session_id())));
+		
+		$this->assertTrue($amount == $amountNew);
+		
+		$this->dispatch('Playlist/delete/name/'.$name);
+	}
+	
+	public function testDeletePlaylists()
+	{
+		$playlist = new Playlist();
+		$session = new Session();
+		$name = 'TestPlaylist';
+		
+		$this->dispatch('Playlist/create/name/'.$name);
+		
+		//does the playlist exists?
+		$this->assertTrue($playlist->existsForUser($name, $session->getUserID(session_id())));
+		
+		$this->dispatch('Playlist/delete/name/'.$name);
+		
+		$this->assertFalse($playlist->existsForUser($name, $session->getUserID(session_id())));
+	
+	}
+	
+	public function testRenamePlaylists()
+	{
+		$playlist = new Playlist();
+		$session = new Session();
+		$name = 'TestPlaylist';
+		$newName = 'NewTestPlaylistName';
+		
+		$this->dispatch('Playlist/create/name/'.$name);
+		
+		//does the playlist exists?
+		$this->assertTrue($playlist->existsForUser($name, $session->getUserID(session_id())));
+		
+		$this->dispatch('Playlist/rename/name/'.$name.'/newname/'.$newName);
+		
+		$this->assertFalse($playlist->existsForUser($name, $session->getUserID(session_id())));
+		$this->assertTrue($playlist->existsForUser($newName, $session->getUserID(session_id())));
+		
+		$this->dispatch('Playlist/delete/name/'.$newName);
+	
+	}
+	
+	public function testRenamePlaylistsDoublicate()
+	{
+		$playlist = new Playlist();
+		$session = new Session();
+		$name = 'TestPlaylist';
+		$newName = 'NewTestPlaylistName';
+		
+		$this->dispatch('Playlist/create/name/'.$name);
+		$this->dispatch('Playlist/create/name/'.$newName);
+		
+		//does the playlist exists?
+		$this->assertTrue($playlist->existsForUser($name, $session->getUserID(session_id())));
+		$this->assertTrue($playlist->existsForUser($newName, $session->getUserID(session_id())));
+		
+		$this->dispatch('Playlist/rename/name/'.$name.'/newname/'.$newName);
+		
+		$this->assertTrue($playlist->existsForUser($name, $session->getUserID(session_id())));
+		$this->assertTrue($playlist->existsForUser($newName, $session->getUserID(session_id())));
+		
+		$this->dispatch('Playlist/delete/name/'.$name);
+		$this->dispatch('Playlist/delete/name/'.$newName);
+	
+	}
+	
 	
 	public function tearDown()
 	{
